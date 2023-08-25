8f63b13b9a2c55f4d05fcfbc2ebc79258e77e1e2
Aleksandr Scupakov <scupakov.aleksandr@gmail.com>
2023-08-25 11:39:04 +0300

    added db schema + data

---
+++ src/main/java/com/aleksandr/blogca2023/entity/Topic.java
@@ -0,0 +1,31 @@
+package com.aleksandr.blogca2023.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    @Entity
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+
+    public class Topic {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        @Column
+        private String title;
+        @Column
+        private String header;
+
+        @OneToMany(mappedBy = "topic", orphanRemoval = true)
+        private List<Comment> comments = new ArrayList<>();
+
+
+
+    }
